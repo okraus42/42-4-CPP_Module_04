225ae9cad8708b17983738615caaec2ac35ef45f
Ondřej Kraus <okraus42+github@pm.me>
2024-06-22 16:56:06 +0200

    ex03, first take

---
+++ ex03/main.cpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: okraus <okraus@student.42prague.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/15 10:52:35 by okraus            #+#    #+#             */
+/*   Updated: 2024/06/22 15:42:28 by okraus           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "colours.hpp"
+
+//https://www.geeksforgeeks.org/shallow-copy-and-deep-copy-in-c/
+
+int main(void)
+{
+	{
+		// Subject main
+		IMateriaSource* src = new MateriaSource();
+		src->learnMateria(new Ice());
+		src->learnMateria(new Cure());
+		ICharacter* me = new Character("me");
+		AMateria* tmp;
+		tmp = src->createMateria("ice");
+		me->equip(tmp);
+		tmp = src->createMateria("cure");
+		me->equip(tmp);
+		ICharacter* bob = new Character("bob");
+		me->use(0, *bob);
+		me->use(1, *bob);
+		delete bob;
+		delete me;
+		delete src;
+		return 0;
+	}
+	return (0);
+}
